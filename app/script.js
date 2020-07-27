9b7c7430448d277675b223a8f86ba67e52b5d296
yuyuan luo <luo.yuyuan@yahoo.com>
2020-07-27 10:28:17 -0400

    api call for data
    API call for covid data. Right now logged in console, saved to variable data. Usable in the draw function.

--- app/script.js
+++ app/script.js
@@ -11,9 +11,23 @@ let backgroundColor,
   mouseCircleDist,
   H, numberOfObstacles,
   obstacle1, obstacle2, obstacle3, obstacle4, obstacle5, obstacle6, obstacle7, obstacle8, obstacle9, obstacle10, hit, winHit,
-    gameOver, button;
+  gameOver, button;
+let data;
 
 function setup() {
+  //api call to get corona data 
+  var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://www.trackcorona.live/api/cities",
+    "method": "GET"
+  }
+
+  $.ajax(settings).done(function (response) {
+    data = response;
+    console.log(data);
+  });
+
   // Canvas & color settings
   width = windowWidth;
   height = windowHeight;
@@ -27,26 +41,29 @@ function setup() {
     x: 100,
     y: 100
   };
-  rectPosition = { x: 150, y: 200 };
+  rectPosition = {
+    x: 150,
+    y: 200
+  };
   H = 0;
-  
+
   hit = false;
-  gameOver= false;
-  winHit= false;
-  
+  gameOver = false;
+  winHit = false;
+
   obstacle1 = new obstacles();
-  obstacle2= new obstacles();
+  obstacle2 = new obstacles();
   obstacle3 = new obstacles();
-  obstacle4= new obstacles();
+  obstacle4 = new obstacles();
   obstacle5 = new obstacles();
-  obstacle6= new obstacles();
+  obstacle6 = new obstacles();
   obstacle7 = new obstacles();
-  obstacle8= new obstacles();
+  obstacle8 = new obstacles();
   obstacle9 = new obstacles();
-  obstacle10= new obstacles();
-  
-    // backgroundColor = (
-    // 4 * (100 - 100 * sigmoid((4 * mouseCircleDist) / (width + height))));
+  obstacle10 = new obstacles();
+
+  // backgroundColor = (
+  // 4 * (100 - 100 * sigmoid((4 * mouseCircleDist) / (width + height))));
   gameOver = false;
 }
 
@@ -55,48 +72,51 @@ function draw() {
     100,
     100
   );
-  
-  if (!gameOver)  {
-  backgroundColor = (
-    4 * (100 - 100 * sigmoid((4 * mouseCircleDist) / (width + height))));
-  strokeWeight(0);
-  fill(backgroundColor, 100, 100);
-  ellipse(spherePosition.x, spherePosition.y, 20, 20);
-  rect(rectPosition.x, rectPosition.y, 20, 20);
-  line(spherePosition.x, spherePosition.y, rectPosition.x, rectPosition.y);
-  squareCircleDist = calculateDistance(spherePosition, rectPosition);
 
-  fill(0);
-  text(
-    `the distance between the square and circle is ${squareCircleDist}`,
-    5,
-    15
-  );
+  if (!gameOver) {
+    backgroundColor = (
+      4 * (100 - 100 * sigmoid((4 * mouseCircleDist) / (width + height))));
+    strokeWeight(0);
+    fill(backgroundColor, 100, 100);
+    ellipse(spherePosition.x, spherePosition.y, 20, 20);
+    rect(rectPosition.x, rectPosition.y, 20, 20);
+    line(spherePosition.x, spherePosition.y, rectPosition.x, rectPosition.y);
+    squareCircleDist = calculateDistance(spherePosition, rectPosition);
 
-  let mousePosition = { x: mouseX, y: mouseY };
-  mouseCircleDist = calculateDistance(spherePosition, mousePosition);
-  text(
-    `the distance between the mouse and circle is ${mouseCircleDist}`,
-    5,
-    30
-  );
-  
-  obstacle1.show();
-  obstacle2.show();
-  obstacle3.show();
-  obstacle4.show(); 
-  obstacle5.show();
-  obstacle6.show();
-  obstacle7.show(); 
-  obstacle8.show();
-  obstacle9.show();
-  obstacle10.show();
-  
-  strokeWeight(10);
-  point(mouseX,mouseY);
-  
-  collisions();
-  
+    fill(0);
+    text(
+      `the distance between the square and circle is ${squareCircleDist}`,
+      5,
+      15
+    );
+
+    let mousePosition = {
+      x: mouseX,
+      y: mouseY
+    };
+    mouseCircleDist = calculateDistance(spherePosition, mousePosition);
+    text(
+      `the distance between the mouse and circle is ${mouseCircleDist}`,
+      5,
+      30
+    );
+
+    obstacle1.show();
+    obstacle2.show();
+    obstacle3.show();
+    obstacle4.show();
+    obstacle5.show();
+    obstacle6.show();
+    obstacle7.show();
+    obstacle8.show();
+    obstacle9.show();
+    obstacle10.show();
+
+    strokeWeight(10);
+    point(mouseX, mouseY);
+
+    collisions();
+
   }
 }
 
@@ -117,30 +137,30 @@ function calculateDistance(coordinate1, coordinate2) {
 function sigmoid(t) {
   return 1 / (1 + Math.pow(Math.E, -t));
 }
-  
 
-class obstacles  {
-  constructor()  {
+
+class obstacles {
+  constructor() {
     this.x = random(width);
     this.y = random(height);
     this.s = random(2, 20);
   }
-  
-  show()  {
+
+  show() {
     fill(220, 80, 80);
     ellipse(this.x, this.y, this.s);
-   
-    hit = collidePointCircle(mouseX,mouseY,this.x,this.y,this.s)
-   
-    if (hit)  {
-      gameOver=true;
+
+    hit = collidePointCircle(mouseX, mouseY, this.x, this.y, this.s)
+
+    if (hit) {
+      gameOver = true;
     }
-     
-    if (gameOver==true){
+
+    if (gameOver == true) {
       background(100);
       textSize(20);
       fill(0);
-      text("Game Over", width/2, height/2);
+      text("Game Over", width / 2, height / 2);
       button = createButton('Try again!');
       button.position(100, 100);
       button.mousePressed(gameOver = false);
@@ -148,12 +168,12 @@ class obstacles  {
   }
 }
 
-function collisions()  {
-  winHit=collidePointCircle(mouseX,mouseY,spherePosition.x,spherePosition.y,20);
-  if (winHit)  {
+function collisions() {
+  winHit = collidePointCircle(mouseX, mouseY, spherePosition.x, spherePosition.y, 20);
+  if (winHit) {
     background(100);
     textSize(30);
-    text("You found the circle!", width/2, height/2);
+    text("You found the circle!", width / 2, height / 2);
     fill(0);
     ellipse(spherePosition.x, spherePosition.y, 20, 20);
   }
