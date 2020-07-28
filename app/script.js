5c7b7171d6e8e7464a937b3dba0721d56366a819
Albert Zhang <32472855+alzh9000@users.noreply.github.com>
2020-07-28 13:18:07 -0400

    added 1 to the d so that more cities would show up as having covid

--- app/script.js
+++ app/script.js
@@ -3,30 +3,27 @@ let myMap;
 let canvas;
 const mappa = new Mappa('Leaflet');
 
-
 function preload() {
 	let url = 'https://www.trackcorona.live/api/cities';
 	data = loadJSON(url);
 }
 
-
 const options = {
 	lat: 0,
 	lng: 0,
 	zoom: 4,
-	style: "http://{s}.tile.osm.org/{z}/{x}/{y}.png"
-}
-
+	style: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+};
 
 function setup() {
 	canvas = createCanvas(640, 640);
 	myMap = mappa.tileMap(options);
 	myMap.overlay(canvas);
 	fill(200, 100, 100);
+	stroke(200, 100, 100)
 
 	myMap.onChange(drawPoint);
-	console.log(data.data)
-
+	console.log(data.data);
 }
 
 // We can use the sigmoid function to scale the cases?
@@ -34,21 +31,20 @@ function sigmoid(t) {
 	return 1 / (1 + Math.pow(Math.E, -t));
 }
 
-function draw() {
-
-}
+function draw() {}
 
 function drawPoint() {
 	// background(backgroundColor, 100, 100);
 	clear();
 
 	for (let i = 0; i < data.data.length; i++) {
-
-		const point = myMap.latLngToPixel(data.data[i].latitude, data.data[i].longitude);
-		let mag = data.data[i].confirmed / 700
+		const point = myMap.latLngToPixel(
+			data.data[i].latitude,
+			data.data[i].longitude
+		);
+		let mag = data.data[i].confirmed / 500;
 		let magmax = sqrt(pow(10, 6));
-		let d = map(mag, 0, magmax, 0, 180);
+		let d = 1 + map(mag, 0, magmax, 0, 180);
 		ellipse(point.x, point.y, d, d);
 	}
-
 }
