558448a936da4e968dae08dbc40388c39f0d11d1
yuyuan luo <luo.yuyuan@yahoo.com>
2020-07-30 10:28:00 -0400

    working map

--- script.js
+++ script.js
@@ -2,48 +2,46 @@ let data;
 let myMap;
 let canvas;
 const mappa = new Mappa('Leaflet');
-let locationData;
+let curLat, curLang;
 
 function preload() {
 	let url = 'https://www.trackcorona.live/api/cities';
 	data = loadJSON(url);
-	locationData = getCurrentPosition();
-	// navigator.geolocation.getCurrentPosition((position) => {
-	// 	curLat = position.coords.latitude
-	// 	curLang = position.coords.longitude;
+	//locationData = getCurrentPosition();
+	navigator.geolocation.getCurrentPosition((position) => {
+		curLat = position.coords.latitude
+		curLang = position.coords.longitude;
+		let options = {
+			lat: curLat,
+			lng: curLang,
+			zoom: 7,
+			style: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+		};
 
-	//});
+		canvas = createCanvas(640, 640);
+		myMap = mappa.tileMap(options);
+		myMap.overlay(canvas);
+		fill(200, 100, 100);
+		stroke(200, 100, 100)
+
+
+		console.log(data.data);
+		myMap.onChange(drawPoint);
+	});
 }
+
 
 
 function setup() {
 
-	const options = {
-		lat: locationData.latitude,
-		lng: locationData.longitude,
-		zoom: 4,
-		style: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
-	};
-
-	canvas = createCanvas(640, 640);
-	myMap = mappa.tileMap(options);
-	myMap.overlay(canvas);
-	fill(200, 100, 100);
-	stroke(200, 100, 100)
-
-	myMap.onChange(drawPoint);
-	console.log(data.data);
 }
 
-// We can use the sigmoid function to scale the cases?
-function sigmoid(t) {
-	return 1 / (1 + Math.pow(Math.E, -t));
-}
 
-function draw() {}
+function draw() {
+
+}
 
 function drawPoint() {
-	// background(backgroundColor, 100, 100);
 	clear();
 
 	for (let i = 0; i < data.data.length; i++) {
